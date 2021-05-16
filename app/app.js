7f9e81bb92c5e239cdb962bc81c0b316a36cd2e9
kisaku <takisaku@heiyou.jp>
2021-05-16 09:35:22 +0900

    fix: details

--- app/app.js
+++ app/app.js
@@ -1,56 +1,21 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
-
-
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
-
-var app = express();
-
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs');
-
-app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
-
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
-
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
-});
-
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
-});
-
-// // db stepup
+const express = require('express');
 const mysql = require('mysql2');
-const db = mysql.createConnection({
+const app = express();
+
+app.use(express.static('public'));
+app.use(express.urlencoded({extended: false}));
+
+const connection = mysql.createConnection({
   host: 'mysql',
   user: 'dev',
   password: 'dev',
   database: 'to_do_list'
 });
 
-db.connect(function(err) {
-  if (err) throw err;
-  console.log('Connected');
+// 以上は設定だからいじらない事を推奨する
+
+app.get('/', (req, res) => {
+  res.render('index.ejs', {title: 'ToDoList'});
 });
 
-module.exports = app;
+app.listen(3000);
